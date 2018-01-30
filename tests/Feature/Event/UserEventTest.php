78870de23885b497a11e54b7290a081a66903a05
Blake Bengtson <bbeng89@gmail.com>
2018-01-29 21:04:28 -0600

    Added test to ensure default shelves are created when a new user is created

--- tests/Feature/Event/UserEventTest.php
+++ tests/Feature/Event/UserEventTest.php
@@ -7,13 +7,30 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserEventTest extends TestCase
 {
+    use RefreshDatabase;
+
+    protected $defaultShelves = [
+        'vinyl' => 'Vinyl',
+        'cassette' => 'Cassette',
+        'cd' => 'CD',
+        'heavy-rotation' => 'Heavy Rotation',
+        'on-deck' => 'On Deck'
+    ];
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testDefaultShelvesAreCreated()
     {
-        $this->assertTrue(true);
+        $user = factory(\App\Models\User::class)->create();
+
+        foreach($this->defaultShelves as $handle => $name)
+        {
+            $shelf = $user->getShelf($handle);
+            $this->assertNotNull($shelf);
+            $this->assertTrue($shelf->name == $name);
+        }
     }
 }
